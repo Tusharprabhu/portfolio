78248827628eee17fead06b2bcea41acad1e5b27
Tusharprabhu <tusharprabhu2004@gmail.com>
2024-04-19 10:34:01 +0530

    comit

---
+++ README.md
@@ -0,0 +1,1 @@
+Kon'nichiwa this is my portfolio website fully made on css html and js
---
+++ main.js
@@ -0,0 +1,17 @@
+
+window= addEventListener('DOMContentLoaded',(e)=>{
+    VANTA.FOG({
+        el: "#fogbag",
+        mouseControls: true,
+        touchControls: true,
+        gyroControls: false,
+        minHeight: 200.00,
+        minWidth: 200.00,
+        highlightColor: 0x7d16bb,
+        midtoneColor: 0x18181d,
+        lowlightColor: 0x381c63,
+        baseColor: 0x0,
+        blurFactor: 0.41,
+        speed: 1.20,
+        zoom: 1.60})
+})
