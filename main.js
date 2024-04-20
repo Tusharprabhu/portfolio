c9a796a93bc768a5030585d493061ed1d5605503
Tusharprabhu <tusharprabhu2004@gmail.com>
2024-04-20 14:59:17 +0530

    commit

--- main.js
+++ main.js
@@ -1,5 +1,4 @@
-
-window= addEventListener('DOMContentLoaded',(e)=>{
+window.addEventListener('DOMContentLoaded', (e) => {
     VANTA.FOG({
         el: "#fogbag",
         mouseControls: true,
@@ -7,11 +6,12 @@ window= addEventListener('DOMContentLoaded',(e)=>{
         gyroControls: false,
         minHeight: 200.00,
         minWidth: 200.00,
-        highlightColor: 0x7d16bb,
-        midtoneColor: 0x18181d,
-        lowlightColor: 0x381c63,
+        highlightColor: 0x4d88b5, // Light blue
+        midtoneColor: 0x004e80,    // Deep blue
+        lowlightColor: 0x002d4d,   // Darker blue
         baseColor: 0x0,
-        blurFactor: 0.41,
-        speed: 1.20,
-        zoom: 1.60})
-})
+        blurFactor: 0.57,
+        speed: 1.10,
+        zoom: 3.0
+    });
+});
